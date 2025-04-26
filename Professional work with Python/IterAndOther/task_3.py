e94a6b116f75435e5164137ebb879c0369b3f318
Spiro <makskedr00@gmail.com>
2025-04-26 05:56:54 +0300

    Iterators task 3

--- Professional work with Python/IterAndOther/task_3.py
+++ Professional work with Python/IterAndOther/task_3.py
@@ -1,15 +1,28 @@
 class FlatIterator:
 
     def __init__(self, list_of_list):
-        self.list_of_list = list_of_list
+        self.list = list_of_list
 
     def __iter__(self):
-        ...
+        self.cursor = -1
         return self
 
     def __next__(self):
-        ...
-        return item
+        self.cursor += 1
+        self.new_list = []
+
+        if self.cursor == len(self.list):
+            raise StopIteration
+
+        if type(self.list[self.cursor]) is list:
+            for item in list(FlatIterator(self.list[self.cursor])):
+                for i in item:
+                    self.new_list.append(i)
+
+        else:
+            self.new_list.append(self.list[self.cursor])
+
+        return self.new_list
 
 
 def test_3():
@@ -29,4 +42,7 @@ def test_3():
 
 
 if __name__ == '__main__':
-    test_3()
+    list_of_lists_2 = [[['a'], ['b', 'c']], ['d', 'e', [['f'], 'h'], False], [1, 2, None, [[[[['!']]]]], []]]
+    print(list(FlatIterator(list_of_lists_2)))
+
+    # Это мой лучший результат. Я примерно понимаю почему остаются первые листы, но вообще без идей как от них избавиться...
