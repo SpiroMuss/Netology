f370afea1fe6a2c1c05e919e252ba3d257b14f20
Spiro <makskedr00@gmail.com>
2025-07-08 11:19:58 +0300

    orm2 2nd task

--- Django/2.2-databases-2/m2m-relations/articles/admin.py
+++ Django/2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        i = 0
+        for form in self.forms:
+            dictionary = form.cleaned_data
+            if dictionary.get('is_main'):
+                i += 1
+            else:
+                continue
+        if i == 0:
+            raise ValidationError('Выберите главную тему')
+        elif i > 1:
+            raise ValidationError('Главной темой может быть только одна')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 1
+    formset = ScopeInlineFormset
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'published_at']
+    inlines = [ScopeInline, ]
+
+
+
