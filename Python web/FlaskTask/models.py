36af831b0ed2837d6a8b675feb510e19a2e23987
morr <elvinhok@gmail.com>
2025-10-04 04:43:06 +0300

    test github authentification

--- Python web/FlaskTask/models.py
+++ Python web/FlaskTask/models.py
@@ -10,7 +10,7 @@ POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'postgres')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5432')
-POSTGRES_DB = os.getenv('POSTGRES_DB', 'Polygon')
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'polygon')
 
 PG_DSN = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
